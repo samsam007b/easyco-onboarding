cc311184cfc5524011d79b70cf333f96f47eb9f1
samuel baudon <baudonsamuel@gmail.com>
2025-11-07 03:09:04 +0100

    feat: add test page for loading animation

---
+++ app/test-loading/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import LoadingLogo from '@/components/LoadingLogo';
+
+export default function TestLoadingPage() {
+  return (
+    <div className="min-h-screen">
+      <LoadingLogo />
+    </div>
+  );
+}
